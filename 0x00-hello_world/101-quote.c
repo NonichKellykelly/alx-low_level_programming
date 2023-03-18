c02638e045d5502526b0b4741e75d6ad51bf76d8
Ijeoma chinonso kellykelly <chinonsokijeoma@gmail.com>
2023-03-18 16:05:11 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+
 /**
- * main - c program that prints a new line to the standard error
- * 
+ * main - Entry point
  * Return: 1 (sucess)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
