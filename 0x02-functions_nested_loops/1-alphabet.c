5f980f4f214e0d159c1ecbeb47ac5429fef60d55
Ijeoma chinonso kellykelly <chinonsokijeoma@gmail.com>
2023-03-21 13:52:45 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ * prints the alphabet - function that prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
