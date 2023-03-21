d709e697b70498bdc299b4906ac6a13f3eb20bd1
Ijeoma chinonso kellykelly <chinonsokijeoma@gmail.com>
2023-03-21 15:10:46 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,18 @@
-i
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  a function that prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
