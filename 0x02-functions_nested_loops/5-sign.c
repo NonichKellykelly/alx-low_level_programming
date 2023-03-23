3b499b23a0b7b891663fd0cb901587bb1cc9972b
Ijeoma chinonso kellykelly <chinonsokijeoma@gmail.com>
2023-03-23 03:54:23 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - function that computes the absolute value of an integer
+ *
+ * @n: parameter to be checked
+ *
+ * Return: 1,0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else (n = 0);
+		_putchar('0');
+		return (0);
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
